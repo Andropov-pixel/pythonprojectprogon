2183d68b5c8f10f52b39389ae676595621396324
артем андропов <line.adsa@bk.ru>
2024-07-21 09:31:57 +0300

    homework_10_2 done

---
+++ tests/conftest.py
@@ -0,0 +1,12 @@
+import pytest
+
+from src.processing import initial_list
+
+
+@pytest.fixture
+def test_initial_list():
+    return 'EXECUTED'
+
+@pytest.fixture
+def test_initial_list_1():
+    return initial_list
