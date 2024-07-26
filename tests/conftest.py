fc55b694046640cb59d3ad822911b260026bfc24
артем андропов <line.adsa@bk.ru>
2024-07-26 09:42:14 +0300

    homework_11_1 done

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,11 +1,12 @@
 import pytest
 
+from src.generators import transactions
 from src.processing import initial_list
 
 
 @pytest.fixture
 def test_initial_list():
-    return 'EXECUTED'
+    return "EXECUTED"
 
 
 @pytest.fixture
@@ -16,3 +17,8 @@ def test_initial_list_1():
 @pytest.fixture
 def date():
     return "2018-07-11T02:26:18.671407"
+
+
+@pytest.fixture
+def test_transactions():
+    return transactions
