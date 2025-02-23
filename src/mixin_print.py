b927a956af26fe8a0fa1ebb4955a7e1fa70b90b3
артем андропов <line.adsa@bk.ru>
2025-02-23 11:41:07 +0300

    homework_16.2_main.py

---
+++ src/mixin_print.py
@@ -0,0 +1,12 @@
+class MixinPrint:
+    """Класс миксин для вывода в консоль информацию об объекте"""
+
+    def __init__(self):
+        self.quantity = None
+        self.price = None
+        self.description = None
+        self.name = None
+        print(repr(self))
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}({self.name}, {self.description}, {self.price}, {self.quantity})"
